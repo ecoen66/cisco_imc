0c718c4dc9e678c4ece6b7ada3e9f879fa5d688b
Ed Coen <ecoen@yahoo.com>
2022-02-18 15:58:16 -0600

    Delete tests directory

--- tests/__int__.py
+++
@@ -1,1 +0,0 @@
-"""Tests for Cisco IMC integration."""
--- tests/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for imc_monitor tests."""
-from homeassistant.const import (
-    CONF_IP_ADDRESS,
-    CONF_USERNAME,
-    CONF_PASSWORD,
-)
-
-# Mock config data to be used across multiple tests
-MOCK_CONFIG = {CONF_IP_ADDRESS: "192.168.1.1", CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
